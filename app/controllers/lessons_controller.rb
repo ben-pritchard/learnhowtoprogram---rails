737b29b704489f9f0d396ce511e3e9a38d145e8b
Ben <ben.s.pritchard@gmail.com>
2015-03-17 11:58:11 -0700

    Add ability for user to add, edit and update lessons, and only permit name and content attributes to be altered.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,4 +6,36 @@ class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(params[:id])
   end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      redirect_to lessons_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(params[:lesson])
+      redirect_to lessons_path
+    else
+      render :edit
+    end
+  end
+
+private
+  def lesson_params
+    params.require(:lesson).permit(:name, :content)
+  end
+
 end
