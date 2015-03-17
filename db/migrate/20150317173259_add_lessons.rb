6d213d7d3bc6e896a8862129762f3edb3e49bab5
Ben <ben.s.pritchard@gmail.com>
2015-03-17 10:54:36 -0700

    Initialize files and create lessons table.

---
+++ db/migrate/20150317173259_add_lessons.rb
@@ -0,0 +1,10 @@
+class AddLessons < ActiveRecord::Migration
+  def change
+    create_table :lessons do |t|
+      t.column :name, :string
+      t.column :content, :string
+
+      t.timestamps
+    end
+  end
+end
